f2cb515b430d1fab21bd146fcb725fee48738936
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-05-27 00:57:52 +0200

    htms + js

--- assets/table3.js
+++ assets/table3.js
@@ -1,8 +1,8 @@
 // Generating a second table with n columns and fill it with some data
 const headers3 = generateHeaders(63, '');
 const data3 = [
-    ['[effet]              =>', ''],
-    ['[targeted_file] =>','']
+    ['<strong>[c_monster.ini]</strong> (86 pipes)', ''],
+    ['[targeted_file]','']
 
 ]; // Insérer vos données ici
 createTable(2, 63, headers3, data3, 'tablesContainer');
