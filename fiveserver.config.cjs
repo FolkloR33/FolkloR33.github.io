311afd6525793f19dd0ee1814eb0a685f516d15a
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-05-29 23:27:12 +0200

    .

--- fiveserver.config.cjs
+++ fiveserver.config.cjs
@@ -3,6 +3,5 @@ module.exports = {
     injectBody: true, // enable instant update
     remoteLogs: true, // enable remoteLogs
     remoteLogs: "yellow", // enable remoteLogs and use the color yellow
-    injectCss: false, // disable injecting css
     navigate: true, // enable auto-navigation
   };
