9558cf652c1f870549655315facc68a608ec7ee1
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-06-03 18:45:02 +0200

    nouveau template sider left

---
+++ assets/js/gf.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Fonction pour charger et insérer le contenu du template
+    function loadTemplate(templateId, targetElement) {
+        fetch(templateId + '.html')
+            .then(response => response.text())
+            .then(data => {
+                const template = document.createElement('template');
+                template.innerHTML = data;
+                const content = template.content;
+                document.querySelector(targetElement).appendChild(content);
+            });
+    }
+    // Charger et insérer le header
+    loadTemplate('/_header', 'header');
+    // Charger et insérer le aside droit
+    loadTemplate('/_right_aside', 'aside.right');
+    // Charger et insérer le aside droit
+    loadTemplate('/_gf_left_aside', 'aside.left');
+    // Charger et insérer le footer
+    loadTemplate('/_footer', 'footer');
+});
