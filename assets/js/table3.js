4341e75bdecdd0c5f2bd860bfb8b27f4e58b0d73
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-06-05 23:08:58 +0200

    css boutons et onglets

--- assets/js/table3.js
+++ assets/js/table3.js
@@ -1,8 +1,8 @@
 // Generating a second table with n columns and fill it with some data
-const headers3 = generateHeaders(63, '');
+const headers3 = generateHeaders(87, '');
 const data3 = [
     ['<strong>[c_monster.ini]</strong> (86 pipes)', ''],
     ['[targeted_file]','']
 
 ]; // Insérer vos données ici
-createTable(2, 63, headers3, data3, 'tablesContainer');
+createTable(2, 87, headers3, data3, 'tablesContainer');
