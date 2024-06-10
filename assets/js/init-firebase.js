79c8b4e88a12c6bc40cdcd79983a0f07899542fa
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-06-10 22:19:32 +0200

    it works !

--- assets/js/init-firebase.js
+++ assets/js/init-firebase.js
@@ -3,7 +3,7 @@ var firebaseConfig = {
     apiKey: "AIzaSyC8xmYTu9BJerJS6n_bAAPnLZWa6wv7Ew4",
     authDomain: "optigf.firebaseapp.com",
     projectId: "optigf-d466f",
-      
+    storageBucket: "optigf-d466f.appspot.com"
   };
   
   // Initialize Firebase
