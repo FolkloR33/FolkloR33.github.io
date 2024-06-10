34b3693b7936481fa4118268b2aff4fb43b6ad33
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-06-10 20:20:09 +0200

    try3

---
+++ assets/js/init-firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    
+    apiKey: "AIzaSyC8xmYTu9BJerJS6n_bAAPnLZWa6wv7Ew4",
+    authDomain: "optigf.firebaseapp.com",
+    projectId: "optigf-d466f",
+      
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
