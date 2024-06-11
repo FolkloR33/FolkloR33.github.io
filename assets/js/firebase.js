d6e6ad329c6e0bd4a6a2851fb41a6e8373b5f571
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-06-11 13:37:58 +0200

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,4 +1,8 @@
-
+var firebaseConfig {
+    apiKey: "AIzaSyC8xmYTu9BJerJS6n_bAAPnLZWa6wv7Ew4",
+    authDomain: "optigf.firebaseapp.com",
+    projectId: "optigf-d466f",
+    storageBucket: "optigf-d466f.appspot.com"
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
