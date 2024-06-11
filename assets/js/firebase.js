ee425750f4fa47f7748c0507f11129840b989495
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-06-11 08:59:55 +0200

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,6 @@
 var firebaseConfig = {
     
-    apiKey: "FIREBASE_API",
+    apiKey: "GITHUB_FIREBASE_API",
     authDomain: "optigf.firebaseapp.com",
     projectId: "optigf-d466f",
     storageBucket: "optigf-d466f.appspot.com"
