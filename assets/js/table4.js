e55caf5fd1a50907035d78138e63feae1a62542b
Bulle <146537274+FolkloR33@users.noreply.github.com>
2024-06-17 20:33:09 +0200

    table4 collection ajout colonnes

--- assets/js/table4.js
+++ assets/js/table4.js
@@ -3,7 +3,7 @@
 
 const headers4 = generateHeaders(10, ''); // Générer les en-têtes en commençant à 0
 const data4 = [
-    ['<strong>C_COLLECTION</strong>(mall)<strong> </strong>(9 pipes)', 'id', 'description?','','','','','points archives','obtention','useless?',],
+    ['<strong>C_COLLECTION</strong>(mall)<strong> </strong>(9 pipes)', 'id', 'description?','switcher si catégorie','catégorie (parent)','catégorie (enfant)','N°case','points archives','obtention','useless?',],
     ['[targeted_file]', 'archives', '', '', '', '', '', '', '', '', '',],
 ]; 
 
